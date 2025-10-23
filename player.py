c49ee080518baa04b5c617398eced6d425a495d0
mushycable19 <dawn.ernst@hotmail.com>
2025-10-23 17:05:19 -0500

    Enable W/S movement in Player update method using move(dt)

--- player.py
+++ player.py
@@ -1,11 +1,10 @@
 # player.py
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
 
 class Player(CircleShape):
     def __init__(self, x, y):
-        # Call parent constructor with x, y, and PLAYER_RADIUS
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0  # initial facing direction
 
@@ -20,21 +19,26 @@ class Player(CircleShape):
         return [a, b, c]
 
     def draw(self, screen):
-        # Draw the player as a white triangle outline
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
 
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
+
     def update(self, dt):
-        # Movement and rotation logic will go here later
         keys = pygame.key.get_pressed()
 
+        # Rotation
         if keys[pygame.K_a]:
-            # Rotate left (counterclockwise)
             self.rotate(-dt)
-
         if keys[pygame.K_d]:
-            # Rotate right (clockwise)
             self.rotate(dt)
 
-    def rotate(self, dt):
-        # Rotate the player clockwise
-        self.rotation += PLAYER_TURN_SPEED * dt
+        # Movement
+        if keys[pygame.K_w]:
+            self.move(dt)       # Move forward
+        if keys[pygame.K_s]:
+            self.move(-dt)      # Move backward (reverse direction)
